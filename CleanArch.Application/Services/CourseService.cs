635a7517319eee237307e7ed6b9cec54928007c4
Adrian nawa <alipz4403@gmail.com>
2024-02-03 16:53:28 +0330

    Course Repository

---
+++ CleanArch.Application/Services/CourseService.cs
@@ -0,0 +1,27 @@
+﻿using CleanArch.Application.Interfices;
+using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Interfices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Services
+{
+    public class CourseService : ICourseServices
+    {
+        private ICourseRepository _courseRepository;
+        public CourseService(ICourseRepository courseRepository)
+        {
+            _courseRepository = courseRepository;
+        }
+        public CourseViewModel GetCourses()
+        {
+            return new CourseViewModel()
+            {
+                Courses=_courseRepository.GetCourses()
+            };
+        }
+    }
+}
---
+++ CleanArch.Infra.Data/Repository/CourseRepository.cs
@@ -0,0 +1,24 @@
+﻿using CleanArch.Domain.Interfices;
+using CleanArch.Domain.Models;
+using CleanArch.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.Data.Repository
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private TestDb _tdb;
+        public CourseRepository(TestDb test)
+        {
+            _tdb = test;
+        }
+        public IEnumerable<Course> GetCourses()
+        {
+            return _tdb.Courses;
+        }
+    }
+}
