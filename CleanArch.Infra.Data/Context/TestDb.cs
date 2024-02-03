f1d512f743bdccca882b8dc13870d6e8e7fb782a
Adrian nawa <alipz4403@gmail.com>
2024-02-03 15:01:20 +0330

    Create Course Model and Context

---
+++ CleanArch.Infra.Data/Context/TestDb.cs
@@ -0,0 +1,20 @@
+﻿using CleanArch.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.Data.Context
+{
+    public class TestDb:DbContext
+    {
+        public TestDb(DbContextOptions<TestDb>options):base(options)
+        {
+            
+        }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
